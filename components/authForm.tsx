2d9783fde4d1e195e4df8da8d15242a8033d133b
Jordi <jordibustos193771@alumnos.info.unlp.edu.ar>
2022-10-26 14:01:52 -0300

    Main - Add signin singup form and functionallity

---
+++ components/authForm.tsx
@@ -0,0 +1,60 @@
+import { Box, Flex, Input, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { FC, useState } from "react";
+import { useSWRConfig } from "swr";
+import NextImage from "next/image";
+import { auth } from "../lib/mutations";
+
+const AuthForm: FC<{ mode: "signin" | "signup" }> = ({ mode }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const user = await auth(mode, { email, password });
+    setIsLoading(false);
+    router.push("/");
+  };
+
+  return (
+    <Box h="100vh" w="100vw" bg="black" color="white">
+      <Flex
+        justify="center"
+        align="center"
+        h="100px"
+        borderBottom="1px solid white"
+      >
+        <NextImage src="/logo.svg" alt="logo" width={120} height={100} />
+      </Flex>
+      <Flex justify="center" align="center" h="calc(100vh - 100px)">
+        <Box padding="50px" bg="gray.900" borderRadius="6px">
+          <form onSubmit={handleSubmit}>
+            <Input
+              placeholder="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              bg="green.500"
+              isLoading={isLoading}
+              sx={{ "&:hover": { bg: "green.300" } }}
+            >
+              {mode}
+            </Button>
+          </form>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default AuthForm;
