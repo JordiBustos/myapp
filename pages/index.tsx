53f2d6a0a5078bb9344e27ac3970d56f677b9a28
Jordi <jordibustos193771@alumnos.info.unlp.edu.ar>
2022-10-27 18:59:51 -0300

    Add user/playlist UI layou

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from "../components/gradientLayout";
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <GradientLayout
+      color="blue"
+      subtitle="profile"
+      title="Jordi Bustos"
+      description="15 public playlists"
+      image="https://sm.ign.com/ign_es/gallery/r/rick-and-m/rick-and-morty-season-5-gallery_6vj7.jpg"
+      roundImage
+    >
+      <div>home page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
